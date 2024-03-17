ba5342dcf428d3700f06ec73e58dd19307009ca4
yuval2017 <hitter@post.bgu.ac.il>
2024-03-17 12:24:35 +0200

    part 2.2.2 is done

--- Part2/Part2.1.py
+++ Part2/Part2.1.py
@@ -2,14 +2,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib_venn import venn3
 
-# Use the venn2 function
 
 if __name__ == "__main__":
     filter_column = ['ReadSequences', 'miRNA', 'Read_start_5', 'Read_end_5', 'mRNA', 'Read_start_3', 'Read_end_3',
                      'Binding_Region']
 
     # Define file paths
-    excel_file_path = '../Data/Table_S2.xlsx'
+
     save_csv_path = '../Data/Table_S2.csv'
 
     # Load data into a Pandas DataFrame
@@ -17,48 +16,49 @@ if __name__ == "__main__":
     # filter columns
     df = df[filter_column]
 
-    binding_regions = ['5’UTR', 'CDS', '3’UTR']
-    filtered_df = df[df['Binding_Region'].isin(binding_regions)]
+    # Define the values you want to keep
+    values_to_keep = ["5'UTR", "CDS", "3'UTR"]
+    for region in values_to_keep:
+        print(f"Total number of interactions per miRNA and mRNA in region {region}:",
+              len(df[df["Binding_Region"] == region]))
+    # Filter the DataFrame to keep rows with specified values
+    filtered_df = df[df['Binding_Region'].isin(values_to_keep)]
 
-    # Compute the total number of interactions per miRNA and mRNA per region
-    interactions_per_region = filtered_df.groupby(['miRNA', 'mRNA', 'Binding_Region']).size().reset_index(
-        name='Total_Interactions')
+    counts_miRNA = filtered_df.groupby(['miRNA', 'Binding_Region']).size().reset_index(name='count')
+    counts_mRNA = filtered_df.groupby(['mRNA', 'Binding_Region']).size().reset_index(name='count')
 
-    # Sum up the total interactions for each selected miRNA and mRNA
-    total_interactions_per_miRNA = interactions_per_region.groupby('miRNA')['Total_Interactions'].sum()
-    total_interactions_per_mRNA = interactions_per_region.groupby('mRNA')['Total_Interactions'].sum()
+    # Pivot the table to create a new DataFrame with miRNA as rows, Binding_Region as columns, and counts as values
+    counts_pivot_miRNA = counts_miRNA.pivot_table(index='miRNA', columns='Binding_Region', values='count', fill_value=0)
+    counts_pivot_mRNA = counts_mRNA.pivot_table(index='mRNA', columns='Binding_Region', values='count', fill_value=0)
 
+    top_20_miRNAs_indexes = counts_pivot_miRNA.sum(axis=1).nlargest(20).index
+    top_20_mRNAs_indexes = counts_pivot_mRNA.sum(axis=1).nlargest(20).index
 
-    top_20_interactions_per_miRNA = total_interactions_per_miRNA.nlargest(20)
-    top_20_interactions_per_mRNA = total_interactions_per_mRNA.nlargest(20)
-    print("Total Interactions per Top 20 miRNAs:")
-    print(top_20_interactions_per_miRNA)
-    print("\nTotal Interactions per Top 20 mRNAs:")
-    print(top_20_interactions_per_mRNA)
+    top_20_counts_miRNA = counts_pivot_miRNA.loc[top_20_miRNAs_indexes]
+    top_20_counts_mRNA = counts_pivot_mRNA.loc[top_20_mRNAs_indexes]
 
-    # # Plot total interactions for top 20 miRNAs
-    # plt.figure(figsize=(12, 6))
-    # top_20_interactions_per_miRNA.plot(kind='bar', color='blue')
-    # plt.title('Total Interactions per Top 20 miRNAs')
-    # plt.xlabel('miRNA')
-    # plt.ylabel('Total Interactions')
-    # plt.xticks(rotation=45, ha='right')
-    # plt.tight_layout()
-    # plt.show()
+    bar_colors = ['blue', 'green', 'red']
+    fig, axes = plt.subplots(2, 1, figsize=(12, 10))
+
+    #top_20_counts_miRNA.plot(kind='bar', ax=axes[0], stacked=True, color=bar_colors)
+    #top_20_counts_mRNA.plot(kind='bar', ax=axes[1], stacked=True, color=bar_colors)
+
+    # # Set titles and axis labels
+    # axes[0].set_title('Top 20 miRNA Interactions by Binding Region')
+    # axes[0].set_xlabel('miRNA')
+    # axes[0].set_ylabel('Total Number Of Interactions')
     #
-    # # Plot total interactions for top 20 mRNAs
-    # plt.figure(figsize=(12, 6))
-    # top_20_interactions_per_mRNA.plot(kind='bar', color='green')
-    # plt.title('Total Interactions per Top 20 mRNAs')
-    # plt.xlabel('mRNA')
-    # plt.ylabel('Total Interactions')
-    # plt.xticks(rotation=45, ha='right')
-    # plt.tight_layout()
+    # axes[1].set_title('Top 20 mRNA Interactions by Binding Region')
+    # axes[1].set_xlabel('mRNA')
+    # axes[1].set_ylabel('Total Number Of Interactions')
+    #
+    # plt.tight_layout()  # Adjust layout to prevent overlap of labels
+    # plt.show()
+    # plt.subplots(figsize=(12, 10))
+    # top_miRNA_5UTR = set(counts_pivot_miRNA.nlargest(20, "5'UTR").index)
+    # top_miRNA_3UTR = set(counts_pivot_miRNA.nlargest(20, "3'UTR").index)
+    # top_miRNA_CDS = set(counts_pivot_miRNA.nlargest(20, "CDS").index)
+    # venn3([top_miRNA_5UTR, top_miRNA_3UTR, top_miRNA_CDS], values_to_keep)
+    # plt.title("Overlap between top miRNAs at different regions")
     # plt.show()
 
-    # Group by miRNA and Binding_Region and count the interactions
-    interactions_per_miRNA_region = filtered_df.groupby(['Binding_Region', 'miRNA']).size()
-
-    # Print the number of interactions for each miRNA and region
-    print("Number of Interactions per miRNA and Region:")
-    print(interactions_per_miRNA_region)
--- Part2/Part2.2.py
+++ Part2/Part2.2.py
@@ -1,9 +1,9 @@
 import re
 import pandas as pd
-from ViennaRNA import RNA
 import subprocess
-
-
+from Bio import pairwise2
+from Bio.Align import substitution_matrices
+import RNA
 def extract_miRNA_fragment_5(row):
     read_sequence = row['ReadSequences']
     start_index = row['Read_start_5']
@@ -113,22 +113,37 @@ def duplex_string(structure, miRNA, mRNA):
 def compute_RNA_duplex(row):
     miRNA = row['miRNA fragment (5\'-3\')']
     mRNA = row['mRNA fragment (3\'-5\')']
+    seq = row["ReadSequences"]
     out, structure, energy, start_miRNA, end_miRNA, start_mRNA, end_mRNA = RNA_duplex(miRNA, mRNA)
 
     if structure == '.&.':
-        return out, "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA"
+        return out, "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA"
 
     miRNA_interacting_region = miRNA[start_miRNA - 1: end_miRNA]
     mRNA_interacting_region = mRNA[start_mRNA - 1: end_mRNA][::-1]
     duplex, matches, bp_gc, mismatch, b_miRNA = duplex_string(structure, miRNA_interacting_region,
                                                               mRNA_interacting_region)
-    return out, miRNA_interacting_region, mRNA_interacting_region, duplex, energy, matches, bp_gc, mismatch, b_miRNA
+    conservation_score = sequence_conservation_score(miRNA, mRNA)
+    normalized_accessibility, mfe = calculate_accessibility(seq)
+    return out, miRNA_interacting_region, mRNA_interacting_region, duplex, energy, matches, bp_gc, mismatch, b_miRNA, conservation_score, normalized_accessibility, mfe
+
+def sequence_conservation_score(miRNA_sequence, mRNA_sequence):
+    # Perform global pairwise alignment using a DNA substitution matrix
+    alignments = pairwise2.align.globalds(miRNA_sequence, mRNA_sequence, substitution_matrices.load("BLOSUM62"), -10, -0.5)
+
+    # Calculate sequence identity as the number of matches divided by the alignment length
+    alignment = alignments[0]
+    alignment_length = len(alignment[0])
+    num_matches = sum(1 for i in range(alignment_length) if alignment[0][i] == alignment[1][i])
+    sequence_identity = num_matches / alignment_length
+    return sequence_identity
+
 
 
 def exec_and_save_df(df, csv_path):
     filter_columns = ["miRNA", "mRNA", "miRNA fragment (5\'-3\')", "mRNA fragment (3\'-5\')", "RNAduplex output",
                       "miRNA interacting region(5'-3')", "mRNA interacting region(3'-5')", "Duplex", "Energy", "BP",
-                      "GC_BP", "mismatches", "miRNA_b"]
+                      "GC_BP", "mismatches", "miRNA_b", "Conservation Score", "Normalized Accessibility", "MFE Value"]
     # Apply the function to each row and create a new column
     df['miRNA fragment (5\'-3\')'] = df.apply(extract_miRNA_fragment_5, axis=1)
     df['mRNA fragment (3\'-5\')'] = df.apply(extract_mRNA_fragment_3, axis=1)
@@ -136,13 +151,76 @@ def exec_and_save_df(df, csv_path):
     # Apply the function to each row and create a new column
     df['RNAduplex output'] = df.apply(compute_RNA_duplex, axis=1)
     df['RNAduplex output'], df["miRNA interacting region(5'-3')"], df["mRNA interacting region(3'-5')"], df['Duplex'], \
-    df['Energy'], df['BP'], df['GC_BP'], df['mismatches'], df["miRNA_b"] = zip(*df.apply(compute_RNA_duplex, axis=1))
+    df['Energy'], df['BP'], df['GC_BP'], df['mismatches'], df["miRNA_b"], df["Conservation Score"], df["Normalized Accessibility"], df["MFE Value"] = zip(*df.apply(compute_RNA_duplex, axis=1))
     df = df[filter_columns]
     # Save the DataFrame to a CSV file
     df.to_csv(csv_path, index=False)
 
+def calculate_accessibility(rna_sequence):
+    # Predict the minimum free energy (MFE) secondary structure
+    (structure, mfe) = RNA.fold(rna_sequence)
+
+    # Calculate the accessibility based on the base pairs in the secondary structure
+    accessibility = 0
+    for i in range(len(structure)):
+        if structure[i] == '.':
+            accessibility += 1
+
+    # Normalize the accessibility score by the length of the sequence
+    normalized_accessibility = accessibility / len(rna_sequence)
+
+    return normalized_accessibility, mfe
+
+from collections import Counter
+
+def calculate_kmer_features(rna_sequence, k):
+    # Initialize a Counter to store the frequency of each k-mer
+    kmer_counter = Counter()
+
+    # Iterate over the RNA sequence to extract k-mers
+    for i in range(len(rna_sequence) - k + 1):
+        # Extract the k-mer of length k starting at position i
+        kmer = rna_sequence[i:i + k]
+        # Increment the count for this k-mer
+        kmer_counter[kmer] += 1
+
+    # Normalize the counts to obtain frequencies
+    total_kmers = sum(kmer_counter.values())
+    kmer_features = {kmer: count / total_kmers for kmer, count in kmer_counter.items()}
+
+    return kmer_features
+
+def most_frequent_kmer(df, k):
+    # Initialize a Counter to store the occurrence counts of k-mers
+    kmer_counts = Counter()
+
+    # Iterate over each sequence in the DataFrame
+    for sequence in df['ReadSequences']:
+        # Calculate the occurrence of k-mers for the current sequence
+        for i in range(len(sequence) - k + 1):
+            kmer = sequence[i:i + k]
+            kmer_counts[kmer] += 1
+
+    # Find the most frequent k-mer
+    most_common_kmer = kmer_counts.most_common(1)
+
+    return most_common_kmer
 
 if __name__ == "__main__":
+    # Example usage
+
+
+
+    rna_sequence = "AUUGCCAGUUCGAUUCGGAAUUCGU"
+
+    k = 3  # Length of k-mer
+    #kmer_features = calculate_kmer_features(rna_sequence, k)
+    accessibility = calculate_accessibility(rna_sequence)
+    seq_miRNA = "AAGCTGCCAGTTGAAGAACTGT"
+    seq_mRNA = "TTACTTCATGGCAGCTATCCCACAG"
+    ans1 = sequence_conservation_score(seq_miRNA, seq_mRNA)
+    print(ans1)
+    #ans2 = calculate_conservation_score([seq_miRNA, seq_mRNA])
     # Your string
     value = ".((((((((..(((((.&.))))))))))))).   1,17  :   3,17  (-19.70)"
     val1, val2 = value.split(':')
@@ -159,10 +237,14 @@ if __name__ == "__main__":
     print(example[22:47])
     # Load data into a Pandas DataFrame
     df = pd.read_csv(save_csv_path)
-
+    ans = 0
+    for path in ['../Data/5UTR_features.csv', '../Data/CDS_features.csv', '../Data/3UTR_features.csv']:
+        ans += len(pd.read_csv(path))
+    #most_frequent_kmer(df, 3)
     df_5UTR = df[df['Binding_Region'] == "5'UTR"].copy()
     df_CDS = df[df['Binding_Region'] == 'CDS'].copy()
     df_3UTR = df[df['Binding_Region'] == "3'UTR"].copy()
-    exec_and_save_df(df_5UTR, './Data/5UTR_features')
-    exec_and_save_df(df_CDS, './Data/CDS_features')
-    exec_and_save_df(df_3UTR, './Data/3UTR_features')
+    ans2 = len(df_5UTR) + len(df_CDS) + len(df_3UTR)
+    exec_and_save_df(df_5UTR, '../Data/5UTR_features.csv')
+    exec_and_save_df(df_CDS, '../Data/CDS_features.csv')
+    exec_and_save_df(df_3UTR, '../Data/3UTR_features.csv')
